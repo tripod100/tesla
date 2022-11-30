0883410d33b55a57adc6cf193e3e0be0ad0d68ab
dara <dayoagbeluyi@yahoo.com>
2022-11-30 17:25:38 +0000

    deployment to staging area

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ we should deploy the new script in 4 weeks
 we hopemake our customers happy and extend the run of projects we have with them
 deployment 4
 deploying all new codes
+deploy to staging
 

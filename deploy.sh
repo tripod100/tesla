e3cae0beb24c2000837275d4ad83299e4df82382
dara <dayoagbeluyi@yahoo.com>
2022-11-30 18:39:41 +0000

    stressing2

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 we should deploy the new script in 4 weeks
 we hopemake our customers happy and extend the run of projects we have with them
 deployment 4
-deploying all new code
+deploying all new codes
 deploy to staging
 echo "good job"
 tetsed and accepted
+
 
 

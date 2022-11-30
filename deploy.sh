f7042da43f2cabca2d306fdcfa0177643ab8c63a
dara <dayoagbeluyi@yahoo.com>
2022-11-30 17:31:13 +0000

    conflict 2 resolved

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 we should deploy the new script in 4 weeks
 we hopemake our customers happy and extend the run of projects we have with them
 deployment 4
-deploying all new codes
+deploying all new code
+deploy to staging
+echo "good job"
+tetsed and accepted
+
 

c1f33d55395635febcf88402fc8085a89dd4f587
Olugbenga Agbeluyi <agbeluyigbenga@gmail.com>
2022-11-30 20:51:16 -0700

    changes made in stage

--- list.java
+++ list.java
@@ -6,26 +6,6 @@ Ope
 sandra
 Bunmi
 Sesan
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 gbenga
 Olu
-=======
-lactose
-chair table
->>>>>>> Stashed changes
-=======
-gbenga
-Olu
->>>>>>> Stashed changes
-=======
-Lactose
-table 
 
->>>>>>> Stashed changes
-=======
-gbenga
-Olu
->>>>>>> Stashed changes
